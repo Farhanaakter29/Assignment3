cc716d95267725a7aa45ea9babc0a876be218c75
farhana akter <farhanaakter120223@gmail.com>
2024-04-16 01:16:29 +0600

    E-5.7

---
+++ src/OrderCompare.java
@@ -0,0 +1,20 @@
+//exercise 5.7
+import java.util.*;
+public class OrderCompare{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter the third number: ");
+        int num3 = sc.nextInt();
+        if (num1 < num2 && num2 < num3){
+            System.out.println("Increasing");
+        } else if (num1 > num2 && num2 > num3){
+            System.out.print("Decreasing");
+        } else {
+            System.out.print("Neither");
+        }
+    }
+}
